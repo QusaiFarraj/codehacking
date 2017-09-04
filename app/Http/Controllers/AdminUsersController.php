887010828187e47f57c5e54afb89be4e5298bbe1
Qusai <Qusai@Flamotechs.com>
2017-09-04 11:10:27 -0500

    users with photos uploading done

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -52,10 +53,32 @@ class AdminUsersController extends Controller
     {
         //
 
-        User::create($request->all());
+       // User::create($request->all());
+
+
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            // to move the image from the browser to the server
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $input['password'] = bcrypt($input['password']);
+
+        User::create($input);
 
         return redirect('/admin/users');
-//        return $request->all();
+
+
+
     }
 
     /**
